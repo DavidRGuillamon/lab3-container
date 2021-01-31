b0d7727b244c6ddc039007cb332ca8ff36bd631a
David Ruiz <d.ruizguillamon@um.es>
2021-01-31 14:01:24 +0100

    Arreglo de la evaluacion con singularity

--- data/k-mer14.py
+++ data/k-mer14.py
@@ -1,4 +1,4 @@
-#!/bin/python3.6
+#!/usr/bin/env python
 
 def convert(c):
 	if (c == 'A'): return 'C'
--- submit_eval_singularity.sh
+++ submit_eval_singularity.sh
@@ -8,19 +8,27 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-user=d.ruizguillamon@um.es
 
+echo "Cargando modulo Singularity."
+module load singularity/3.7.0
+
+
 echo "Ejecutando script k-mer13 en Python con Singularity."
-(time ./data/k-mer13.py) 2>&1
+(time singularity exec ./singularity/pythonImage.sif ./data/k-mer13.py) 2>&1
 echo "======================================"
 echo "======================================"
 echo "Ejecutando script k-mer13 en C++ con Singularity."
-(time ./data/k-mer13) 2>&1
+(time singularity exec ./singularity/pythonImage.sif ./data/k-mer13) 2>&1
 echo "======================================"
 echo "======================================"
 echo "Ejecutando script k-mer14 en Python con Singularity."
-(time ./data/k-mer14.py) 2>&1
+(time singularity exec ./singularity/pythonImage.sif ./data/k-mer14.py) 2>&1
 echo "======================================"
 echo "======================================"
 echo "Ejecutando script k-mer14 en C++ con Singularity."
-(time ./data/k-mer14) 2>&1
+(time singularity exec ./singularity/pythonImage.sif ./data/k-mer14) 2>&1
 echo "======================================"
 echo "Trabajo terminado." 
+
+
+echo "Descargando modulo Singularity."
+module unload singularity/3.7.0
